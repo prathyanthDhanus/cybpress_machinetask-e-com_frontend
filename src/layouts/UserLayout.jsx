1e3dc2ede6c293e9d44d70fe341e6cf040f8926e
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2025-01-12 11:20:42 +0530

    feat: create initial project outline

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const UserLayout = () => {
+  return (
+    <>
+    <div>
+        
+    </div>
+
+    <div>
+            <Outlet/>
+        </div>
+
+        <div>
+            
+        </div>
+    </>
+  )
+}
+
+export default UserLayout
